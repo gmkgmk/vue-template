027fbb6e4d7f8a42420b9c6b0d7b2ae401de5b9c
gmkgmk <18408271384@sina.cn>
2020-03-16 23:04:39 +0800

    更改dll为splitChunks

--- vue.config.js
+++ vue.config.js
@@ -1,74 +1,101 @@
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
 const resolve = dir => path.join(__dirname, dir);
 const isProduction = process.env.NODE_ENV === 'production';
-const webpack = require('webpack');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const pageTitle = 'Vue-element模版';
 
 module.exports = {
+  outputDir: 'dist',
   productionSourceMap: false,
   parallel: require('os').cpus().length > 1,
-  configureWebpack: {
-    plugins: [
-      new AddAssetHtmlPlugin({
-        filepath: path.resolve(__dirname, './public/dll/*.js'), // dll文件位置
-        publicPath: '/dll', // dll 引用路径
-        outputPath: '/dll' // dll最终输出的目录
-      })
-    ]
+  publicPath: '/',
+  pages: {
+    index: {
+      entry: resolve('/src/main.js'),
+      title: pageTitle,
+      chunks: ['chunk-libs', 'chunk-elementUI', 'chunk-commons', 'runtime', 'index']
+    }
   },
   chainWebpack: config => {
     config.resolve.alias.set('@pages', resolve('/src/pages'));
     config.resolve.alias.set('@services', resolve('/src/services'));
     config.resolve.alias.set('@component', resolve('/src/component'));
 
-    config.plugins.delete('prefetch'); // TODO: need test
-    config.plugins.delete('preload'); // TODO: need test
+    ['index'].forEach(entryName => {
+      config.plugins.delete(`preload-${entryName}`);
+      config.plugins.delete(`prefetch-${entryName}`);
+    });
 
-    // 处理dll **
-    config.plugin('dll').use(webpack.DllReferencePlugin, [
-      {
-        manifest: require('./public/dll/vendors-manifest.json')
-      }
-    ]);
-
-    // 处理runtime
-    config
-      .plugin('ScriptExtHtmlWebpackPlugin')
-      .after('html')
-      .use('script-ext-html-webpack-plugin', [
-        {
-          // `runtime` must same as runtimeChunk name. default is `runtime`
-          inline: /runtime\..*\.js$/
-        }
-      ])
-      .end();
-
-    // 生产模式配置
+    // 开发模式配置
     if (isProduction) {
-      //  启用GZip压缩
-      config
-        .plugin('compression')
-        .use(CompressionPlugin, {
-          asset: '[path].gz[query]',
-          algorithm: 'gzip',
-          test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
-          threshold: 10240,
-          minRatio: 0.8,
-          cache: true
-        })
-        .tap(() => {});
-
-      // 压缩代码
-      config.optimization.minimize(true);
+      // 处理runtime
+      importRuntimeJS(config);
       // 分割代码
-      config.optimization.splitChunks({
-        chunks: 'all'
-      });
-      // 代码引用检查
-      config
-        .plugin('webpack-bundle-analyzer')
-        .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
+      splitChunks(config);
+      //  启用GZip压缩
+      Gzip(config);
+      // 代码引用检查;
+      analyzer(config);
+      // 独立runtimeJs
+      config.optimization.runtimeChunk('single');
     }
   }
 };
+
+function importRuntimeJS(config) {
+  config
+    .plugin('ScriptExtHtmlWebpackPlugin')
+    .after('html')
+    .use('script-ext-html-webpack-plugin', [
+      {
+        // `runtime` must same as runtimeChunk name. default is `runtime`
+        inline: /runtime\..*\.js$/
+      }
+    ])
+    .end();
+}
+
+function Gzip(config) {
+  config
+    .plugin('compression')
+    .use('compression-webpack-plugin', {
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+      threshold: 10240,
+      minRatio: 0.8,
+      cache: true
+    })
+    .tap(() => {});
+}
+
+function analyzer(config) {
+  config
+    .plugin('webpack-bundle-analyzer')
+    .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin);
+}
+
+function splitChunks(config) {
+  config.optimization.splitChunks({
+    chunks: 'all',
+    cacheGroups: {
+      libs: {
+        name: 'chunk-libs',
+        test: /[\\/]node_modules[\\/]/,
+        priority: 10,
+        chunks: 'initial' // only package third parties that are initially dependent
+      },
+      elementUI: {
+        name: 'chunk-elementUI', // split elementUI into a single package
+        priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+        test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+      },
+      commons: {
+        name: 'chunk-commons',
+        test: resolve('src/component'), // can customize your rules
+        minChunks: 3, //  minimum common number
+        priority: 5,
+        reuseExistingChunk: true
+      }
+    }
+  });
+}
